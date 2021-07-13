84bc5801f0c49557d29534f05ba4dbd1cf26b6ca
Prakhar-mehrishi <62169851+Prakhar-mehrishi@users.noreply.github.com>
2021-07-14 01:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,4 @@ def predict():
         
   return render_template('index.html', prediction_text='Model  has predicted  : {}'.format(result))
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run()
