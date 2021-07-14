2503efc8cac90e001dbb690ab5bb155b04995e82
Prakhar-mehrishi <62169851+Prakhar-mehrishi@users.noreply.github.com>
2021-07-14 11:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,93 @@
+import streamlit as st 
+from PIL import Image
+import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-from flask import Flask, request, jsonify, render_template
-from flask_ngrok import run_with_ngrok
-import pickle
-app = Flask(__name__)
-import pickle
-model = pickle.load(open('/content/drive/My Drive/celebal_model.pkl', 'rb'))
-# Importing the dataset
-dataset = pd.read_csv('/content/drive/My Drive/Churn_Modelling.csv')
-# Extracting dependent and independent variables:
-# Extracting independent variable:
-X = dataset.iloc[:,3:13].values
-# Extracting dependent variable:
-y = dataset.iloc[:, 13].values
+st.set_option('deprecation.showfileUploaderEncoding', False)
+# Load the pickled model
+pickle_in = open("/content/drive/My Drive/celebal_model.pkl","rb")
+model=pickle.load(pickle_in)
+dataset= pd.read_csv('/content/drive/My Drive/Churn_Modelling.csv')
+X = dataset.iloc[:,1:10].values
+
+# Taking care of missing data
+#handling missing data (Replacing missing data with the mean value)  
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values= np.NaN, strategy= 'mean', fill_value=None, verbose=1, copy=True)
+#Fitting imputer object to the independent variables x.   
+imputer = imputer.fit(X[:,3:9]) 
+#Replacing missing data with the calculated mean value  
+X[:,3:9]= imputer.transform(X[:,3:9])  
+
+# Taking care of missing data
+#handling missing data (Replacing missing data with the mean value)  
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values= np.NaN, strategy= 'constant', fill_value='female', verbose=1, copy=True)
+#Fitting imputer object to the independent variables x.   
+imputer = imputer.fit(X[:, 2:3]) 
+#Replacing missing data with the constant value  
+X[:, 2:3]= imputer.transform(X[:,2:3])  
+
 # Encoding Categorical data:
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder
 labelencoder_X = LabelEncoder()
 X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+
 # Encoding Categorical data:
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder
 labelencoder_X = LabelEncoder()
 X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
-#dummy encoding.
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-columnTransformer = ColumnTransformer([('New', OneHotEncoder(), [1])],remainder='passthrough')
-X=columnTransformer.fit_transform(X)
-#dummy encoding.
-
- # Dummy Variable trapping
-X = X[:, 1:] 
-# Splitting the Dataset into the Training set and Test set
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-# Feature Scaling
-# Standard Scaling:  Standardization = X'=X-mean(X)/standard deviation
-# normal scaling : Normalization= X'=X-min(X)/max(x)-min(X)
 
 from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-run_with_ngrok(app)
+sc = StandardScaler()
+X = sc.fit_transform(X)
 
-@app.route('/')
-def home():
-  
-    return render_template("index.html")
-  
-@app.route('/predict',methods=['GET'])
-def predict():
-  '''
-  For rendering results on HTML GUI
-  '''
-  creditscore = int(request.args.get('creditscore'))
-  geo = int(request.args.get('geo'))
-  age = int(request.args.get('age'))
-  tenure = int(request.args.get('tenure'))  
-  balance = int(request.args.get('balance'))
-  numofproducts = int(request.args.get('numofproducts')) 
-  creditcards=int(request.args.get('creditcards'))
-  activemember = int(request.args.get('activemember'))
-  
-  salary = int(request.args.get('salary')) 
-  
-  
-  y_pred= model.predict(sc_X.transform(np.array([[0,1, creditscore,geo,age,tenure,balance,
-                                                  numofproducts,creditcards,activemember,salary]])))
-  y_pred = (y_pred > 0.5)
-  if y_pred>0.5:
-    result="Customer will not exit Bank"
+def predict_note_authentication(CreditScore,Geography,Gender,Age,Tenure,Balance,HasCrCard,IsActiveMember,EstimatedSalary):
+  output= model.predict(sc.transform([[CreditScore,Geography,Gender,Age,Tenure,Balance,HasCrCard,IsActiveMember,EstimatedSalary]]))
+  print("Customer will leave =", output)
+  if output==[1]:
+    prediction="Customer will Leave"
   else:
-    result="Customer will exit bank"
-        
-  return render_template('index.html', prediction_text='Model  has predicted  : {}'.format(result))
+    prediction="Customer will not Leave"
+  print(prediction)
+  return prediction
+def main():
+    
+    html_temp = """
+   <div class="" style="background-color:blue;" >
+   <div class="clearfix">           
+   <div class="col-md-12">
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Celebal ML Internship</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;">Group - 8, Task-1 Project</p></center> 
+   </div>
+   </div>
+   </div>
+   """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    st.header("Customer Prediction")
+    Age = st.number_input('Insert a Age',18,60)
+    CreditScore= st.number_input('Insert a CreditScore',400,1000)
+    HasCrCard = st.number_input('Insert a HasCrCard 0 For No 1 For Yes',0,1)
+    Tenure = st.number_input('Insert a Tenure',0,20)
+    Balance = st.number_input('Insert a Balance',0)
+    Gender = st.number_input('Insert 0 For Male 1 For Female ',0,1)
+    Geography= st.number_input('Insert Geography 0 For France 1 For Spain'0,1)
+    IsActiveMember= st.number_input('Insert a IsActiveMember 0 For No 1 For Yes',0,1)
+    EstimatedSalary= st.number_input('Insert a EstimatedSalary',0)
+    
+    
+    resul=""
+    if st.button("Predict"):
+      result=predict_note_authentication(CreditScore,Geography,Gender,Age,Tenure,Balance,HasCrCard,IsActiveMember,EstimatedSalary)
+      st.success('Model has predicted {}'.format(result))
+    if st.button("About"):
+      st.subheader("Developed by Team - 8")
+      st.subheader("Celebal ML internship")
 
 if __name__=='__main__':
-    app.run(debug=True)
+  main()
+   
